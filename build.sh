a2b5777efa9b219efa2fdf39c248be8e58525aa2
Stefano Pace <stefanopace01@gmail.com>
2020-09-05 12:50:56 +0200

    added base template

---
+++ build.sh
@@ -0,0 +1,3 @@
+rm -r build
+mkdir -p build/curriculum
+cp src/assets/template.html build/curriculum/index.html
--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 ./build.sh
+ssh stefano@51.210.12.70 "rm -rf /var/www/stefanopace/curriculum"
 scp -r build/curriculum stefano@51.210.12.70:/var/www/stefanopace
