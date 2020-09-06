7577a21bfd7b3444c3bd4950c4bfecde0e8e8522
Stefano Pace <stefanopace01@gmail.com>
2020-09-06 21:24:04 +0200

    implementato build del contenuto tramite file

--- build.py
+++ build.py
@@ -1,5 +1,48 @@
+from os import listdir
+from os.path import isfile, join
+
+def indentation_level(line):
+    level = 0
+    for split in line.split('    '):
+        if split == '':
+            level += 1
+        else:
+            break
+    return level
+
+
+content_path = 'src/content/'
+content_filenames = [f for f in listdir(content_path) if isfile(join(content_path, f))]
+
 with open('src/assets/template.html') as file:
     template = ''.join(file.readlines())
+
+for content_filename in content_filenames:
+    with open(content_path + content_filename) as content_file:
+        content = content_file.readlines()
+    
+    html=['<div>']
+    previous_indentation_level = 0
+    for line in content:
+        current_indentation_level = indentation_level(line)
+        if (current_indentation_level > previous_indentation_level):
+            for i in range(current_indentation_level - previous_indentation_level):
+                html.append('<div>')
+        
+        if (current_indentation_level < previous_indentation_level):
+            for i in range(previous_indentation_level - current_indentation_level):
+                html.append('</div>')
+
+        if (current_indentation_level == 0):
+            html.append('<b class="fucsia">'+line+'</b><br>')
+        else:
+            html.append(line+'<br>')
+        previous_indentation_level = current_indentation_level
+
+    for i in range(previous_indentation_level):
+        html.append('</div>')
+    html.append('</div><br>')
+    template = template.replace('%'+content_filename+'%', ''.join(html))
 
 index = template
 
