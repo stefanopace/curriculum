4135973386c6eaddb8f15b9bf946e14bcae31522
Stefano Pace <stefanopace01@gmail.com>
2020-09-12 21:17:24 +0200

    moved deploy to site root

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 rm -r build
 mkdir -p build/curriculum
 python3 build.py
+cp src/assets/favicon.ico build/curriculum/
--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 ./build.sh
-ssh stefano@51.210.12.70 "rm -rf /var/www/stefanopace/curriculum"
+ssh stefano@51.210.12.70 "rm -rf /var/www/stefanopace"
 scp -r build/curriculum stefano@51.210.12.70:/var/www/stefanopace
